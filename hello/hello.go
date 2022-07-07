928593c9875301d30b2215d79834d7663efba840
CalesvolChen <calesvolchen@gmail.com>
2022-07-07 12:54:10 +0800

    init

---
+++ hello/hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+
+	"log"
+
+	"example.com/greetings"
+)
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// message, err := greetings.Hello("Calesvol")
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Println(message)
+
+	names := []string{"Calesvol", "何欣媛"}
+	messages, errs := greetings.Hellos(names)
+
+	if errs != nil {
+		log.Fatal(errs)
+	}
+
+	fmt.Println(messages)
+}
